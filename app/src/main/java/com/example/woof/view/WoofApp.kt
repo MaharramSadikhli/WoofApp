12fa8bf424d722ac33da6ff6fa4991ecf412fd0b
maharram sadikhli <s.muarem10@gmail.com>
2024-07-30 11:08:03 +0400

    created top app bar

--- app/src/main/java/com/example/woof/view/WoofApp.kt
+++ app/src/main/java/com/example/woof/view/WoofApp.kt
@@ -3,24 +3,44 @@ package com.example.woof.view
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.dp
 import com.example.woof.R
 import com.example.woof.data.dogs
+import com.example.woof.ui.components.TopAppBarRow
 
 /**
  * Composable that displays an app bar and a list of dogs.
  */
 @Composable
 fun WoofApp() {
-    
+
     val paddingSmall = dimensionResource(id = R.dimen.padding_small)
-    
-    LazyColumn(modifier = Modifier.padding(top = 30.dp)) {
-        items(dogs) {
-            DogItem(dog = it, modifier = Modifier.padding(paddingSmall))
+
+    Scaffold(
+        topBar = { WooTopAppBar() }
+    ) {
+        LazyColumn(contentPadding = it) {
+            items(dogs) { dog ->
+                DogItem(dog = dog, modifier = Modifier.padding(paddingSmall))
+            }
         }
     }
+}
+
+/**
+ * Composable that displays a top app bar.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WooTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            TopAppBarRow()
+        }, modifier = modifier
+    )
 }
